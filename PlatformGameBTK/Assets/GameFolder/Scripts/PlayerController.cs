880c202b9ab4d807eba1afd6c0815fabc95f9218
Yusuf Kaan SARI <yusufkaansari@hotmail.com>
2023-06-29 13:56:47 +0300

    Tiles, Background, Player Run Animation

---
+++ PlatformGameBTK/Assets/GameFolder/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D playerRB;
+    Animator playerAnimator;
+
+    [SerializeField]
+    float moveSpeed = 1f;
+
+    bool facingRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalMove();
+
+        if (playerRB.velocity.x < 0 && facingRight)
+        {
+            FlipFace();
+        }
+        else if (playerRB.velocity.x > 0 && !facingRight)
+        {
+            FlipFace();
+        }
+    }
+
+    void HorizontalMove()
+    {
+        playerRB.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, playerRB.velocity.y);
+        playerAnimator.SetFloat("playerSpeed", Mathf.Abs(playerRB.velocity.x));
+    }
+    void FlipFace()
+    {
+        facingRight = !facingRight;
+        Vector3 tempLocalScale = transform.localScale;
+        tempLocalScale.x *= -1;
+        transform.localScale = tempLocalScale;
+    }
+}
